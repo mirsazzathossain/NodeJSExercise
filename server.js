fe9e70d87032b8266feea65d7301996c5ca7ba56
cis101 <cis101@secs.iub.edu.bd>
2019-11-04 09:37:08 +0600

    node 1st !!

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var http = require('http');
+
   var server = http.createServer(function(req, res){
+
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     res.end("Hello World\n");
