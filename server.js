c8f65858e8c3e390df531f0c4fef7890c6592e97
cis101 <cis101@secs.iub.edu.bd>
2019-11-04 09:45:25 +0600

    Exercise 02!

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 var http = require('http');
-
+  var fs = require('fs');
   var server = http.createServer(function(req, res){
 
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end("Hello World\n");
+    fs.readFile('index.html', function(err, data){
+      if(err){
+        res.statusCode = 404;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('File not found');
+      }
+
+
+      res.setHeader('Content-Type', 'text/html');
+      res.statusCode = 200;
+      res.end(data);
+    });
+    
+    
   });
   server.listen(3000, 'localhost', function(){
     console.log('Server running');
