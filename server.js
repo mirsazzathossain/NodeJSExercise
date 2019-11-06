edd1af8f95936f259e84a7968c918a6461768b29
cis101 <cis101@secs.iub.edu.bd>
2019-11-06 09:11:34 +0600

    Classwork 8b!!

--- server.js
+++ server.js
@@ -1,22 +1,18 @@
 var http = require('http');
-  var fs = require('fs');
-  var server = http.createServer(function(req, res){
-
-    fs.readFile('index.html', function(err, data){
-      if(err){
-        res.statusCode = 404;
-        res.setHeader('Content-Type', 'text/plain');
-        res.end('File not found');
-      }
+  var express = require('express');
+  var app = express();
+  var server = http.Server(app);
 
 
-      res.setHeader('Content-Type', 'text/html');
-      res.statusCode = 200;
-      res.end(data);
-    });
-    
-    
+  app.get('/',function(request,response){
+    response.sendFile(__dirname+'/index.html');
   });
+
+
+  app.get('/second',function(request,response){
+    response.sendFile(__dirname+'/index_2.html')
+  })
+  
   server.listen(3000, 'localhost', function(){
     console.log('Server running');
   });
