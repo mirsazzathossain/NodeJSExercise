fd7cee549e769ec95a10537be686545ad9ff3c02
mirsazzathossain <mirsazzathossain@gmail.com>
2019-11-06 09:47:04 +0600

    Classwork 8c!!

--- server.js
+++ server.js
@@ -1,18 +1,30 @@
 var http = require('http');
-  var express = require('express');
-  var app = express();
-  var server = http.Server(app);
+var express = require('express');
+var app = express();
+var server = http.Server(app);
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.post('/new_article', function(request, response){
+  console.log(request.body);
+  response.json({msg: "Successfully recieved"});
+})
 
 
-  app.get('/',function(request,response){
-    response.sendFile(__dirname+'/index.html');
-  });
+app.get('/',function(request,response){
+  response.sendFile(__dirname+'/index.html');
+});
 
 
-  app.get('/second',function(request,response){
-    response.sendFile(__dirname+'/index_2.html')
-  })
-  
-  server.listen(3000, 'localhost', function(){
-    console.log('Server running');
-  });
+app.get('/second',function(request,response){
+  response.sendFile(__dirname+'/index_2.html')
+});
+
+app.get('/new_article',function(request,response){
+  response.sendFile(__dirname+'/form.html');
+});
+
+server.listen(3000, 'localhost', function(){
+  console.log('Server running');
+});
