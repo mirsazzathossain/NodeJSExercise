8afa07c7c167cce0e9328a6c8a36f46a2bedcb79
cis101 <cis101@secs.iub.edu.bd>
2019-11-13 09:20:06 +0600

    Classwork 9a!!

--- server.js
+++ server.js
@@ -3,12 +3,33 @@ var express = require('express');
 var app = express();
 var server = http.Server(app);
 var bodyParser = require('body-parser');
+var mongo = require('mongodb');
+
+var db, uri = "mongodb+srv://mirsazzathossain:sazzatmir@cluster0-wdd8v.mongodb.net/test?retryWrites=true&w=majority"
+
+mongo.MongoClient.connect(uri,
+  {useNewUrlParser: true, useUnifiedTopology: true},
+  function(err, client){
+    if(err){
+      console.log('Could not connect to MongoDB')
+    }else{
+      db = client.db('node-cw9')
+    }
+
+  });
+
+var save = function(form_data){
+  db.createCollection('articles', function(err, collections){})
+  var collection = db.collection('articles')
+  collection.save(form_data)
+}
 
 app.use(bodyParser.urlencoded({extended:true}));
 
 let articles=[];
 
 app.post('/new_article', function(request, response){
+  save(request.body);
   articles.push(request.body);
   console.log(articles);
   response.json({msg: "Successfully recieved"});
