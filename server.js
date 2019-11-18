e6a220b33be8da6b3b9433a3da6a3d9805f1c189
mirsazzathossain <mirsazzathossain@gmail.com>
2019-11-18 09:49:27 +0600

    Classwork 9b!!

--- server.js
+++ server.js
@@ -4,9 +4,35 @@ var app = express();
 var server = http.Server(app);
 var bodyParser = require('body-parser');
 var mongo = require('mongodb');
+var mongoose = require('mongoose');
 
-var db, uri = "mongodb+srv://mirsazzathossain:sazzatmir@cluster0-wdd8v.mongodb.net/test?retryWrites=true&w=majority"
+var db, url = "mongodb+srv://mirsazzathossain:sazzatmir@cluster0-wdd8v.mongodb.net/test?retryWrites=true&w=majority"
 
+mongoose.connect(url,
+  {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connection.on('error', function(err){
+  console.log("Could not connect to MongoDB")
+})
+
+var Schema = mongoose.Schema
+var articleSchema = new Schema({
+      title: {
+          type: String,
+          required: "must require title"
+      },
+      content: {
+        type: String, 
+        required: "must require content"
+      }
+    }
+)
+
+var Article = mongoose.model('Article',articleSchema)
+
+
+
+
+/* 
 mongo.MongoClient.connect(uri,
   {useNewUrlParser: true, useUnifiedTopology: true},
   function(err, client){
@@ -17,22 +43,32 @@ mongo.MongoClient.connect(uri,
     }
 
   });
+ */
 
+/* 
 var save = function(form_data){
   db.createCollection('articles', function(err, collections){})
   var collection = db.collection('articles')
   collection.save(form_data)
 }
+ */
 
 app.use(bodyParser.urlencoded({extended:true}));
 
 let articles=[];
 
 app.post('/new_article', function(request, response){
-  save(request.body);
-  articles.push(request.body);
-  console.log(articles);
-  response.json({msg: "Successfully recieved"});
+  //save(request.body);
+  //articles.push(request.body);
+  let article = new Article(request.body);
+  article.save(function(err, data){
+    if(err){
+      return response.status(400).json({msg: "All fields are required!"});
+    }
+    return response.status(200).json({article:data});
+  })
+  //console.log(articles);
+  //response.json({msg: "Successfully recieved"});
 });
 
 app.get('/article/:index', function(request, response){
